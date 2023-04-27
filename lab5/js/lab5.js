a4a66065a355761309ee8d0e1b80f97746429892
Jackmac1234 <jbax143@gmail.com>
2023-04-26 21:03:34 -0700

    Lab 5
    Ye we did some stuff with javascript. Ye.

---
+++ lab5/js/lab5.js
@@ -0,0 +1,38 @@
+// index.js - Lab 5
+// Author: John McCarthy
+// Date: April 26, 2023
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
+
+// Define Variables
+  var make = "Buick";
+  var model = "Enclave";
+  var color = "Black";
+  var year = 2014;
+
+// Calcualte
+  var age = 2023 - year;
+
+// Output 
+  document.writeln("Make: " + make + "<br>");
+  document.writeln("Model: " + model + "<br>");
+  document.writeln("Color: " + color + "<br>");
+  document.writeln("Year: " + year + "<br>");
+  document.writeln("Age: " + age + " years<br>");
+  
